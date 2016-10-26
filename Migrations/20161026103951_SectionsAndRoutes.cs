e67afb69ebdceb26789f266e68d09b5a424681af
Mathias Jakobsen <mathias@jbcoding.com>
2016-10-26 13:01:20 +0200

    Add database for route and section classes

---
+++ Migrations/20161026103951_SectionsAndRoutes.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AKK.Migrations
+{
+    public partial class SectionsAndRoutes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sections",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sections", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Routes",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(nullable: false),
+                    Author = table.Column<string>(nullable: true),
+                    ColorOfHolds = table.Column<uint>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Grade = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    SectionID = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Routes", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Routes_Sections_SectionID",
+                        column: x => x.SectionID,
+                        principalTable: "Sections",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Routes_SectionID",
+                table: "Routes",
+                column: "SectionID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Routes");
+
+            migrationBuilder.DropTable(
+                name: "Sections");
+        }
+    }
+}
--- Models/MainDbContext.cs
+++ Models/MainDbContext.cs
@@ -1,4 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using AKK.Models;
+using System.Collections.Generic;
+using System;
 
 namespace AKK.Models
 {
@@ -7,5 +10,58 @@ namespace AKK.Models
         public MainDbContext(DbContextOptions<MainDbContext> options)
             : base(options)
         {}
+
+        public DbSet<Route> Routes { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Route>().HasOne(r => r.Section)
+                                        .WithMany(s => s.Routes)
+                                        .HasForeignKey(r => r.SectionID);
+        }
+    }
+    public static class DbContextExtensions
+    {
+        public static void Seed(this MainDbContext context)
+        {
+            // Perform database delete and create
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            // Perform seed operations
+            Section s = new Section {Name = "A"};
+            List<Route> routes = new List<Route>();
+            routes.Add(new Route {
+                ID = Guid.NewGuid(),
+                Name = "1",
+                Author = "Mathias Hornumm",
+                Date = DateTime.Now,
+                Grade = Grades.Green,
+                ColorOfHolds = 0x000000FF
+            });
+            routes.Add(new Route {
+                ID = Guid.NewGuid(),
+                Name = "2",
+                Author = "Mathias Hornumm",
+                Date = DateTime.Now,
+                Grade = Grades.Blue,
+                ColorOfHolds = 0xFF0000FF
+            });
+            routes.Add(new Route {
+                ID = Guid.NewGuid(),
+                Name = "3",
+                Author = "Mathias Jakobsen",
+                Date = DateTime.Now,
+                Grade = Grades.White,
+                ColorOfHolds = 0x00FF00FF
+            });
+
+            s.Routes.AddRange(routes);
+            context.Sections.Add(s);
+            
+            // Save changes and release resources
+            context.SaveChanges();
+            context.Dispose();
+        }
     }
 }
