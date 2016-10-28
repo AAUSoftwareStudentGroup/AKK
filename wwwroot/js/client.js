2657aa6625dbfd7a0e5cf7cb22bf3dd9c1a4d64c
Christian Grünberg <cmgrunberg@gmail.com>
2016-10-28 12:58:05 +0200

    RouteClient and SectionClient frontend created

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -1,22 +1,125 @@
 function RouteClient(url)
 {
-    this.getAll = function(grade, section, sortBy, success)
+    this.getRoutes = function(grade, section, sortBy, success)
     {
         $.ajax({
-          dataType: "json",
-          url: url,
-          data: 
-          {  
-              grade : grade,
-              section : section,
-              sortBy : sortBy
-          },
-          success: success
+            type: "GET",
+            dataType: "json",
+            url: url,
+            data: 
+            {  
+                grade: grade,
+                section: section,
+                sortBy: sortBy
+            },
+            success: success
         });
-    }
+    };
+
+    this.getRoute = function(id, success)
+    {
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: url + "/" + id,
+            data:
+            {
+                id: id
+            },
+            success: success
+        });
+    };
+
+    this.addRoute = function(sectionID, name, author, colorOfHolds, grade, success)
+    {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: url,
+            data:
+            {
+                sectionID: sectionID,
+                name: name,
+                author: author,
+                colorOfHolds: colorOfHolds,
+                grade: grade
+            },
+            success: success
+        });
+    };
+
+    this.deleteRoute = function(id, success)
+    {
+        $.ajax({
+            type: "DELETE", 
+            dataType: "json",
+            url: url + "/" + id,
+            data:
+            {
+                id: id
+            },
+            success: success
+        });
+    };
 }
 
-function Client()
+function SectionClient(url)
+{
+    this.getAllSections = function(success)
+    {
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: url,
+            success: success
+        });
+    };
+
+    this.getSection = function(name, success)
+    {
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: url + "/" + name,
+            data:
+            {
+                name: name
+            },
+            success: success
+        });
+    };
+
+        this.addSection = function(name, success)
+    {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: url,
+            data:
+            {
+                name: name
+            },
+            success: success
+        });
+    };
+
+    this.deleteSection = function(name, success)
+    {
+        $.ajax({
+            type: "DELETE", 
+            dataType: "json",
+            url: url + "/" + name,
+            data:
+            {
+                name: name
+            },
+            success: success
+        });
+    };
+}
+
+function Client(routeUrl, sectionUrl)
 {
     this.routes = new RouteClient(API_ROUTE_URL);
+    this.sections = new SectionClient(API_SECTION_URL);
 }
