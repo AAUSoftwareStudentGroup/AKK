0813443e0ea1a19e083a33abfcc26a4090e57dae
Mathias Ibsen <Mathias Ibsen@DESKTOP-NFBHK49>
2016-10-28 14:21:51 +0200

    Add seed method for db

---
+++ Migrations/20161028120722_SeedOnly.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Akk.Migrations
+{
+    public partial class SeedOnly : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
