dd637d9e5f0c454bb6838f263c1db946c33ea1da
JacobSvenningsen <jacobsvenningsen15@hotmail.com>
2016-11-14 16:57:12 +0100

    merge and added Names to each existing Color

--- Classes/Models/Grade.cs
+++ Classes/Models/Grade.cs
@@ -1,14 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq.Expressions;
 using Newtonsoft.Json;
 
 namespace AKK.Classes.Models {
     public class Grade {
-        protected uint? ColorDB { get; set; }
-
         [Key]
         [JsonIgnore]
         public Guid GradeId  { get; set; }
@@ -17,19 +13,9 @@ namespace AKK.Classes.Models {
 
         public int Difficulty { get; set; }
 
-        [NotMapped]
-        public Color Color { 
-            get {
-                return Color.FromUint(ColorDB);
-            } 
-            set {
-                ColorDB = value?.ToUint();
-            } 
-        }
+        public Color Color { get; set; }
 
         [JsonIgnore]
         public virtual List<Route> Routes { get; set; }
-        // https://weblogs.asp.net/ricardoperes/mapping-non-public-members-with-entity-framework-code-first
-        public static readonly Expression<Func<Grade, uint?>> ColorPriv = p => p.ColorDB;  
     }
 }
