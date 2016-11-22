2832059a38b02baf09fddfdd9120311681610fa9
Mathias Ibsen <mathias@ibsen.name>
2016-11-22 10:59:13 +0100

    Viewmodel fix

--- wwwroot/js/route-info.js
+++ wwwroot/js/route-info.js
@@ -1,9 +1,18 @@
 ﻿var viewModel;
+
+function NavigationService() {
+    this.back = function() { window.location.replace("routes.html"); };
+    this.location = function(routeId) {
+        window.location = "edit-route.html?routeId=" + routeId;
+    };
+}
+
 $(document).ready(function () {
     var template = Handlebars.compile($("#route-info-template").html());
     var client = new Client(API_ROUTE_URL, API_SECTION_URL, API_GRADE_URL);
     var changed = function changed() { $('#content').html(template(viewModel)); };
-    viewModel = new RouteInfoViewModel(client, changed);
+
+    viewModel = new RouteInfoViewModel(client, changed, new NavigationService());
 });
 Handlebars.registerHelper('ifCond', function (v1, v2, options) {
     if (v1.g <= v2) {
