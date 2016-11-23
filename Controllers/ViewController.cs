309b5c43211a8d401276d3f735fed75b32d45735
Morten R <morten.rask@live.dk>
2016-11-23 09:09:11 +0100

    Reorder views and navigationService

--- Controllers/ViewController.cs
+++ Controllers/ViewController.cs
@@ -3,29 +3,24 @@ using Microsoft.AspNetCore.Mvc;
 namespace AKK.Controllers {
     [Route("")]
     public class ViewController : Controller {
-        public ViewController()
-        {
-            
-        }
-
         // GET: /
         [HttpGet]
-        public IActionResult Routes() { return View(); }
+        public IActionResult Routes() { return View("Views/Routes.cshtml"); }
 
         // GET: /route-info
         [HttpGet("route-info")]
-        public IActionResult RouteInfo() { return View(); }
+        public IActionResult RouteInfo() { return View("Views/RouteInfo.cshtml"); }
 
         // GET: /new-route
         [HttpGet("new-route")]
-        public IActionResult NewRoute() { return View(); }
+        public IActionResult NewRoute() { return View("Views/NewRoute.cshtml"); }
 
         // GET: /edit-route
         [HttpGet("edit-route")]
-        public IActionResult EditRoute() { return View(); }
+        public IActionResult EditRoute() { return View("Views/EditRoute.cshtml"); }
 
         // GET: /sections
         [HttpGet("sections")]
-        public IActionResult Sections() { return View(); }
+        public IActionResult Sections() { return View("Views/Sections.cshtml"); }
     }
 }
