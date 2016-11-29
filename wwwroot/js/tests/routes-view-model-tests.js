42cfe11d721aedec138bb26e2d2952a0406f73ae
Mathias Ibsen <mathias@ibsen.name>
2016-11-29 19:31:48 +0100

    Included Videos, Rates and Comments in RouteRepository

--- wwwroot/js/tests/routes-view-model-tests.js
+++ wwwroot/js/tests/routes-view-model-tests.js
@@ -1,38 +1,32 @@
 var viewModel;
 var events = ["RoutesChanged", ];
-QUnit.test("Route View Model Tests", function( assert ) {
+QUnit.test("Route View Model Tests", function (assert) {
     viewModel = new RoutesViewModel(new TestClient(API_ROUTE_URL, API_SECTION_URL, API_GRADE_URL, API_MEMBER_URL, new TestCookieService()));
 
     viewModel.init();
 
-    var routesChangedTriggered = false;
-    var selectedSectionTriggered = false;
     var selectedGradeTriggered = false;
+    var filteringChangedTriggered = false;
+    var searchMethodChangedTriggered = false;
 
-    viewModel.addEventListener("routesChanged", function () {
-        routesChangedTriggered = true;
+
+    viewModel.addEventListener("filteringChanged", function () {
+        filteringChangedTriggered = true;
     });
 
-    viewModel.addEventListener("selectedSectionChanged", function () {
-        selectedSectionTriggered = true;
+    viewModel.addEventListener("SearchMethodChanged", function () {
+        searchMethodChangedTriggered = true;
     });
 
     viewModel.changeSection(TEST_SECTIONS[2].id);
 
-    assert.equal(routesChangedTriggered, true, "changeSection routesChanged triggered");
+    viewModel.toggleSearch();
 
-    assert.equal(selectedSectionTriggered, true, "changeSection selectedSectionChanged triggered");
+    assert.equal(filteringChangedTriggered, true, "ToggleSearch filteringChanged triggered");
 
-    var correctSection = true;
+    viewModel.searchClicked();
 
-    for (var i = 0; i < viewModel.routes.length; i++) {
-        correctSection = correctSection && viewModel.routes[i].sectionId == TEST_SECTIONS[2].id
-    }
-
-    if(viewModel.routes.length > 0)
-    {
-        assert.equal(correctSection, true, "changeSection all routes in selected section");
-    }
+    assert.equal(searchMethodChangedTriggered, true, "SearchClick SearchMethodChanged triggered");
 
     viewModel.addEventListener("selectedGradeChanged", function () {
         selectedGradeTriggered = true;
@@ -40,16 +34,42 @@ QUnit.test("Route View Model Tests", function( assert ) {
 
     viewModel.changeGrade(TEST_GRADES[3].id);
 
-    assert.equal(selectedSectionTriggered, true, "changeSection selectedGradeChanged triggered");
-
     var correctGrade = true;
 
     for (var i = 0; i < viewModel.routes.length; i++) {
-        correctGrade = correctGrade && viewModel.routes[i].gradeId == TEST_GRADES[3].id
+        correctGrade = correctGrade && viewModel.routes[i].gradeId == TEST_GRADES[3].id;
     }
 
-    if(viewModel.routes.length > 0)
-    {
+    if (viewModel.routes.length > 0) {
         assert.equal(correctGrade, true, "changeGrade all routes in selected Grade");
     }
 });
+
+QUnit.test("Change Section in Route ViewModel", function (assert) {
+    viewModel = new RoutesViewModel(new TestClient(API_ROUTE_URL, API_SECTION_URL, API_GRADE_URL, API_MEMBER_URL, new TestCookieService()));
+    var routesChangedTriggered = false;
+
+    viewModel.init();
+
+    viewModel.addEventListener("routesChanged", function () {
+        routesChangedTriggered = true;
+    });
+
+    viewModel.changeSection(TEST_SECTIONS[2].id);
+
+    assert.equal(routesChangedTriggered, true, "changeSection routesChanged triggered");
+
+    viewModel.changeSection(TEST_SECTIONS[2].id);
+    var correctSection = true;
+
+    for (var i = 0; i < viewModel.routes.length; i++) {
+        correctSection = correctSection && viewModel.routes[i].sectionId == TEST_SECTIONS[2].id;
+    }
+
+    if (viewModel.routes.length > 0) {
+        assert.equal(correctSection, true, "changeSection all routes in selected section");
+    }
+});
+
+
+
