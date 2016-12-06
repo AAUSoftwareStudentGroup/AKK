60e441b2c076a6431f3914bf9dda12db46e37860
Morten R <morten.rask@live.dk>
2016-12-06 21:51:21 +0100

    Fix missing error message when adding a route without anything selected

--- wwwroot/js/new-route-view-model.js
+++ wwwroot/js/new-route-view-model.js
@@ -29,14 +29,19 @@ function NewRouteViewModel(client, navigationService) {
         }
         var sectionId = (self.selectedSection == null ? null : self.selectedSection.id);
         var gradeId = (self.selectedGrade == null ? null : self.selectedGrade.id);
-        var holdColor = self.selectedHold;
+        var holdColor = null;
+        if(self.selectedHold != null)
+        {
+            holdColor = self.selectedHold.colorOfHolds;
+        }
         var tapeColor = null;
         if (self.selectedTape != null) {
             tapeColor = self.selectedTape.colorOfHolds;
         }
         var routeNumber = self.number;
         var author = self.author;
-        self.client.routes.addRoute(sectionId, routeNumber, author, holdColor.colorOfHolds, gradeId, tapeColor, this.note, imgObject, function(response) {
+
+        self.client.routes.addRoute(sectionId, routeNumber, author, holdColor, gradeId, tapeColor, this.note, imgObject, function(response) {
             if (response.success) {
                 self.navigationService.toRouteInfo(response.data.id);
             } else {
